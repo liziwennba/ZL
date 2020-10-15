dced310b56447b25cfa6dc6c0376e5b3aec9bfab
liziwennba <55771171+liziwennba@users.noreply.github.com>
2020-10-15 19:50:32 +0800

    Add files via upload

--- ece143sample.py
+++ ece143sample.py
@@ -1,4 +1,8 @@
 def write_columns(data,fname):
+    assert type(data)==list
+    assert type(fname)==str
+    for i in data:
+        assert type(i)==int or type(i)==float
     data_value_2=[x**2 for x in data]
     data_value_3=[]
     for i in range(len(data)):
