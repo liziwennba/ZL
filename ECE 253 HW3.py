3d8623572ca1d981e65a38d122efd10e5d8eda96
liziwennba <55771171+liziwennba@users.noreply.github.com>
2020-11-20 19:02:03 +0800

    Update ECE 253 HW3.py

--- ECE 253 HW3.py
+++ ECE 253 HW3.py
@@ -136,139 +136,133 @@ img_back = np.abs(img_back)
 plt.show()
 # '''
 # HW3 question 3
-# '''
-# import torch
-# import torchvision
-# import torchvision.transforms as transforms
-# transform = transforms.Compose(
-#     [transforms.ToTensor(),
-#      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-#
-# trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-#                                         download=True, transform=transform)
-# trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-#                                           shuffle=True, num_workers=0)
-#
-# testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-#                                        download=True, transform=transform)
-# testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-#                                          shuffle=False, num_workers=0)
-#
-# classes = ('plane', 'car', 'bird', 'cat',
-#            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-# import matplotlib.pyplot as plt
-# import numpy as np
-#
-# # functions to show an image
-#
-#
-# def imshow(img):
-#     img = img / 2 + 0.5     # unnormalize
-#     npimg = img.numpy()
-#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-#     plt.show()
-#
-# # # get some random training images
-# # dataiter = iter(trainloader)
-# # images, labels = dataiter.next()
-# #
-# # # show images
-# # imshow(torchvision.utils.make_grid(images))
-# # # print labels
-# # print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-#
-# import torch.nn as nn
-# import torch.nn.functional as F
-#
-#
-# class Net(nn.Module):
-#     def __init__(self):
-#         super(Net, self).__init__()
-#         self.conv1 = nn.Conv2d(3, 6, 5)
-#         self.pool = nn.MaxPool2d(2, 2)
-#         self.conv2 = nn.Conv2d(6, 16, 5)
-#         self.fc1 = nn.Linear(16 * 5 * 5, 120)
-#         self.fc2 = nn.Linear(120, 84)
-#         self.fc3 = nn.Linear(84, 10)
-#
-#     def forward(self, x):
-#         x = self.pool(F.relu(self.conv1(x)))
-#         x = self.pool(F.relu(self.conv2(x)))
-#         x = x.view(-1, 16 * 5 * 5)
-#         x = F.relu(self.fc1(x))
-#         x = F.relu(self.fc2(x))
-#         x = self.fc3(x)
-#         return x
-#
-#
-# net = Net()
-# import torch.optim as optim
-#
-# criterion = nn.CrossEntropyLoss()
-# optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-#
-#
-#
-# loss_plot = []
-# for epoch in range(2):  # loop over the dataset multiple times
-#
-#     running_loss = 0.0
-#     for i, data in enumerate(trainloader, 0):
-#         # get the inputs; data is a list of [inputs, labels]
-#         inputs, labels = data
-#
-#         # zero the parameter gradients
-#         optimizer.zero_grad()
-#
-#         # forward + backward + optimize
-#         outputs = net(inputs)
-#         loss = criterion(outputs, labels)
-#         loss.backward()
-#         optimizer.step()
-#
-#         # print statistics
-#         running_loss += loss.item()
-#         if i % 2000 == 1999:    # print every 2000 mini-batches
-#             print('[%d, %5d] loss: %.3f' %
-#                   (epoch + 1, i + 1, running_loss / 2000))
-#             loss_plot.append(running_loss/2000)
-#             running_loss = 0.0
-#
-# PATH = './cifar_net.pth'
-# torch.save(net.state_dict(), PATH)
-# print('Finished Training')
-# x=[]
-# mini_batch = 2000
-# for i in range(len(loss_plot)):
-#     x.append(mini_batch)
-#     mini_batch +=2000
-# plt.plot(x,loss_plot)
-# plt.title('loss vs number of mini-batch')
-# plt.xlabel('number of mini-batches')
-# plt.ylabel('loss')
-# plt.show()
-#
-#
-#
-# dataiter = iter(testloader)
+import torch
+import torchvision
+import torchvision.transforms as transforms
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=0)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=0)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+import matplotlib.pyplot as plt
+import numpy as np
+
+# functions to show an image
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+# # get some random training images
+# dataiter = iter(trainloader)
 # images, labels = dataiter.next()
 #
-# # print images
+# # show images
 # imshow(torchvision.utils.make_grid(images))
-# print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-#
-# outputs = net(images)
-# _, predicted = torch.max(outputs, 1)
-#
-# print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-#                               for j in range(4)))
-# '''
-# 2.
-# We are using 12500 images and 3125 batches(batch size 4) to train the network
-# 3.
-# Yes, we normalizer the image from [0,1] to [-1,1]. In the example, we first unnormalized the random images from training dataset to [0,1], then transform them to numpy array and use matplot to show the images.
-# 4.
-#
-#
-#
-# '''
+# # print labels
+# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    def first_output(self,x):
+        x = self.conv1(x)
+        return x
+
+
+net = Net()
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+
+loss_plot = []
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            loss_plot.append(running_loss/2000)
+            running_loss = 0.0
+
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+print('Finished Training')
+x=[]
+mini_batch = 2000
+for i in range(len(loss_plot)):
+    x.append(mini_batch)
+    mini_batch +=2000
+plt.plot(x,loss_plot)
+plt.title('loss vs number of mini-batch')
+plt.xlabel('number of mini-batches')
+plt.ylabel('loss')
+plt.show()
+
+
+
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+# print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                              for j in range(4)))
+
+outputs_first = net.first_output(images)
+outputs_first =o
